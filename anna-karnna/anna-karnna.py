f6ecc91db15127ca5339c43d37b3ab1f7782422e
Martin Fabbri <martin.fabbri.a@gmail.com>
2017-11-17 03:06:33 +0000

    OCR scan mini-project.

--- anna-karnna/anna-karnna.py
+++ anna-karnna/anna-karnna.py
@@ -92,8 +92,192 @@ def build_lstm(lstm_size, num_layers, batch_size, keep_prob):
         drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
         return drop
 
-    cell = tf.contrib.rnn.MultipleRNNCell([build_cell(lstm_size, keep_prob) for _ in range(num_layers)])
+    cell = tf.contrib.rnn.MultiRNNCell([build_cell(lstm_size, keep_prob) for _ in range(num_layers)])
     initial_state = cell.zero_state(batch_size, tf.float32)
 
     return cell, initial_state
 
+### Output Layer
+def build_output(lstm_output, in_size, out_size):
+    """
+    Build a softmax layer, return the softmax output and logits.
+    :param lstm_output:
+    :param in_size: Size of the input tensor, for example, size of the LSTM cells
+    :param out_size:
+    :return:
+    """
+    # reshape output so it's a bunch of rows, one row for each step
+    # for concatenate lstm_output over axis 1
+    seq_output = tf.concat(lstm_output, axis=1)
+
+    # reshape seq_output to a 2d tensor with lstm_size columns
+    x = tf.reshape(seq_output, [-1, in_size])
+
+    # connect the rnn output to a softmax layer
+    with tf.variable_scope('softmax'):
+        softmax_w = tf.Variable(tf.truncated_normal((in_size, out_size), stddev=0.1))
+        softmax_b = tf.Variable(tf.zeros(out_size))
+
+    # since output is a bunvh of rows of rnn cell outputs, logits will be a bunch
+    # of rows of logit outputs, one for each step and sequence
+    logits = tf.matmul(x, softmax_w) + softmax_b
+    out = tf.nn.softmax(logits, name='predictions')
+
+    return out, logits
+
+### training loss
+def build_loss(logits, targets, lstm_size, num_classes):
+    y_one_hot = tf.one_hot(targets, num_classes)
+    y_reshaped = tf.reshape(y_one_hot, logits.get_shape())
+
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_reshaped)
+    loss = tf.reduce_mean(loss)
+    return loss
+
+
+def build_optimizer(loss, learning_rate, grad_clip):
+    ''' Build optmizer for training, using gradient clipping.
+
+        Arguments:
+        loss: Network loss
+        learning_rate: Learning rate for optimizer
+
+    '''
+
+    # Optimizer for training, using gradient clipping to control exploding gradients
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), grad_clip)
+    train_op = tf.train.AdamOptimizer(learning_rate)
+    optimizer = train_op.apply_gradients(zip(grads, tvars))
+
+    return optimizer
+
+
+class CharRNN:
+    def __init__(self, num_classes, batch_size=64, num_steps=50,
+                 lstm_size=128, num_layers=2, learning_rate=0.001,
+                 grad_clip=5, sampling=False):
+
+        # When we're using this network for sampling later, we'll be passing in
+        # one character at a time, so providing an option for that
+        if sampling == True:
+            batch_size, num_steps = 1, 1
+        else:
+            batch_size, num_steps = batch_size, num_steps
+
+        tf.reset_default_graph()
+
+        # Build the input placeholder tensors
+        self.inputs, self.targets, self.keep_prob = build_inputs(batch_size, num_steps)
+
+        # Build the LSTM cell
+        cell, self.initial_state = build_lstm(lstm_size, num_layers, batch_size, self.keep_prob)
+
+        ### Run the data through the RNN layers
+        # First, one-hot encode the input tokens
+        x_one_hot = tf.one_hot(self.inputs, num_classes)
+
+        # Run each sequence step through the RNN and collect the outputs
+        outputs, state = tf.nn.dynamic_rnn(cell, x_one_hot, initial_state=self.initial_state)
+        self.final_state = state
+
+        # Get softmax predictions and logits
+        self.prediction, self.logits = build_output(outputs, lstm_size, num_classes)
+
+        # Loss and optimizer (with gradient clipping)
+        self.loss = build_loss(self.logits, self.targets, lstm_size, num_classes)
+        self.optimizer = build_optimizer(self.loss, learning_rate, grad_clip)
+
+
+batch_size = 100        # Sequences per batch
+num_steps = 100         # Number of sequence steps per batch
+lstm_size = 512         # Size of hidden layers in LSTMs
+num_layers = 2          # Number of LSTM layers
+learning_rate = 0.001   # Learning rate
+keep_prob = 0.5
+
+epochs = 20
+# Save every N iterations
+save_every_n = 200
+
+model = CharRNN(len(vocab), batch_size=batch_size, num_steps=num_steps,
+                lstm_size=lstm_size, num_layers=num_layers,
+                learning_rate=learning_rate)
+
+saver = tf.train.Saver(max_to_keep=100)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    # Use the line below to load a checkpoint and resume training
+    # saver.restore(sess, 'checkpoints/______.ckpt')
+    counter = 0
+    for e in range(epochs):
+        # Train network
+        new_state = sess.run(model.initial_state)
+        loss = 0
+        for x, y in get_batches(encoded, batch_size, num_steps):
+            counter += 1
+            start = time.time()
+            feed = {model.inputs: x,
+                    model.targets: y,
+                    model.keep_prob: keep_prob,
+                    model.initial_state: new_state}
+            batch_loss, new_state, _ = sess.run([model.loss,
+                                                 model.final_state,
+                                                 model.optimizer],
+                                                feed_dict=feed)
+
+            end = time.time()
+            print('Epoch: {}/{}... '.format(e + 1, epochs),
+                  'Training Step: {}... '.format(counter),
+                  'Training loss: {:.4f}... '.format(batch_loss),
+                  '{:.4f} sec/batch'.format((end - start)))
+
+            if (counter % save_every_n == 0):
+                saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+
+    saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+
+def pick_top_n(preds, vocab_size, top_n=5):
+    p = np.squeeze(preds)
+    p[np.argsort(p)[:-top_n]] = 0
+    p = p / np.sum(p)
+    c = np.random.choice(vocab_size, 1, p=p)[0]
+    return c
+
+
+def sample(checkpoint, n_samples, lstm_size, vocab_size, prime="The "):
+    samples = [c for c in prime]
+    model = CharRNN(len(vocab), lstm_size=lstm_size, sampling=True)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, checkpoint)
+        new_state = sess.run(model.initial_state)
+        for c in prime:
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab_to_int[c]
+            feed = {model.inputs: x,
+                    model.keep_prob: 1.,
+                    model.initial_state: new_state}
+            preds, new_state = sess.run([model.prediction, model.final_state],
+                                        feed_dict=feed)
+
+        c = pick_top_n(preds, len(vocab))
+        samples.append(int_to_vocab[c])
+
+        for i in range(n_samples):
+            x[0, 0] = c
+            feed = {model.inputs: x,
+                    model.keep_prob: 1.,
+                    model.initial_state: new_state}
+            preds, new_state = sess.run([model.prediction, model.final_state],
+                                        feed_dict=feed)
+
+            c = pick_top_n(preds, len(vocab))
+            samples.append(int_to_vocab[c])
+
+    return ''.join(samples)
+
+checkpoint = tf.train.latest_checkpoint('checkpoints')
+samp = sample(checkpoint, 2000, lstm_size, len(vocab), prime="Far")
+print(samp)
